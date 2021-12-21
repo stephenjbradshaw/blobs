3d731d3b008036e9b4e41cc96730f3df017174cb
Stephen Bradshaw <hello@stephenbradshaw.dev>
2021-12-21 12:19:38 +0000

    Add basic drawing context

---
+++ src/Canvas.js
@@ -0,0 +1,44 @@
+import {useRef, useEffect, useCallback} from "react";
+
+const Canvas = ({...rest}) => {
+  const canvasRef = useRef(null);
+
+  const draw = useCallback((ctx, frameCount) => {
+    const x = 50;
+    const y = 100;
+    const radius = 20 * Math.sin(frameCount * 0.01) ** 2;
+    const startAngle = 0;
+    const endAngle = 2 * Math.PI;
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(x, y, radius, startAngle, endAngle);
+    ctx.fill();
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    let frameCount = 0;
+    let animationFrameId;
+
+    const render = () => {
+      frameCount++;
+      draw(ctx, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
+
+    draw(ctx);
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  }, [draw]);
+
+  return <canvas ref={canvasRef} {...rest} />;
+};
+
+export default Canvas;
