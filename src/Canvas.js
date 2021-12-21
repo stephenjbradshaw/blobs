d0ece93fc9e4f26685b8d79c66d29e566d487c67
Stephen Bradshaw <hello@stephenbradshaw.dev>
2021-12-21 13:06:20 +0000

    Change to time-based animation Otherwise, if animation is based on frames, it could mean that it runs faster on high refresh-rate screens See: https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame#example

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,42 +1,48 @@
-import {useRef, useEffect, useCallback} from "react";
+import {useRef, useEffect} from "react";
 
 const Canvas = ({...rest}) => {
   const canvasRef = useRef(null);
-
-  const draw = useCallback((ctx, frameCount) => {
-    const x = 50;
-    const y = 100;
-    const radius = 20 * Math.sin(frameCount * 0.01) ** 2;
-    const startAngle = 0;
-    const endAngle = 2 * Math.PI;
-
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.arc(x, y, radius, startAngle, endAngle);
-    ctx.fill();
-  }, []);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
-    let frameCount = 0;
+    let initialTimestamp, elapsedTime;
     let animationFrameId;
 
-    const render = () => {
-      frameCount++;
-      draw(ctx, frameCount);
-      animationFrameId = window.requestAnimationFrame(render);
-    };
-    render();
+    const renderFrame = (currentTimestamp) => {
+      // If this is the first frame, store the initial timestamp
+      if (initialTimestamp === undefined) {
+        initialTimestamp = currentTimestamp;
+        // Else, calculate time since animation started – animation progress will be based on this
+      } else {
+        elapsedTime = currentTimestamp - initialTimestamp;
+      }
 
-    draw(ctx);
+      // Circle params
+      const x = 50;
+      const y = 100;
+      const radius = 20 * Math.sin(elapsedTime * 0.001) ** 2;
+      const startAngle = 0;
+      const endAngle = 2 * Math.PI;
+
+      // Draw
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.fillStyle = "#000000";
+      ctx.beginPath();
+      ctx.arc(x, y, radius, startAngle, endAngle);
+      ctx.fill();
+
+      // Step to next frame with recursive call
+      animationFrameId = window.requestAnimationFrame(renderFrame);
+    };
+
+    renderFrame();
 
     return () => {
       window.cancelAnimationFrame(animationFrameId);
     };
-  }, [draw]);
+  }, []);
 
   return (
     <canvas ref={canvasRef} {...rest} style={{border: "1px solid black"}}>
