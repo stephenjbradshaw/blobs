a0454148993dfaed0beefab9ecb01f9da3c4a5c5
Stephen Bradshaw <hello@stephenbradshaw.dev>
2021-12-22 18:47:41 +0000

    Extract ball class to own file

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,61 +1,10 @@
 import {useRef, useEffect} from "react";
-
-const getRandomInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min);
-};
+import { Ball } from "./Ball";
 
 const Canvas = ({...rest}) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    class Ball {
-      constructor(canvas, radius, color) {
-        this.x = getRandomInt(0, canvas.width);
-        this.y = getRandomInt(0, canvas.height);
-        this.radius = radius;
-        this.color = color;
-        this.previousElapsedTime = 0;
-        this.isFirstDraw = true;
-      }
-      draw(canvas, ctx, elapsedTime, velocity) {
-        // Do nothing on first frame, because we need two frames to work out time since last draw
-        if (elapsedTime === 0) return;
-
-        /* Base animation speed on time rather than frames.
-         * This ensures speed stays independent of screen refresh rate, see:
-         * https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
-         */
-        this.timeSinceLastDraw = elapsedTime - this.previousElapsedTime;
-
-        // Set initial speed and direction. Velocity is in pixels-per-second.
-        if (this.isFirstDraw) {
-          this.dx = (this.timeSinceLastDraw / 1000) * velocity;
-          this.dy = (this.timeSinceLastDraw / 1000) * velocity;
-          this.isFirstDraw = false;
-        }
-
-        // Move the drawing position
-        this.x += this.dx;
-        this.y += this.dy;
-
-        // Draw
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fillStyle = this.color;
-        ctx.fill();
-
-        // On collision with edges, flip delta values for the next draw
-        if (this.y + this.dy > canvas.height || this.y + this.dy < 0) {
-          this.dy = -this.dy;
-        }
-        if (this.x + this.dx > canvas.width || this.x + this.dx < 0) {
-          this.dx = -this.dx;
-        }
-
-        this.previousElapsedTime = elapsedTime;
-      }
-    }
 
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
