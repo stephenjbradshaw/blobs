61a1b8a580352adc9c8cb5e96afa6f3a773b96dd
Stephen Bradshaw <hello@stephenbradshaw.dev>
2021-12-22 12:06:07 +0000

    Refactor to base animation speed on time elapsed

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,58 +1,85 @@
 import {useRef, useEffect} from "react";
 
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
 const Canvas = ({...rest}) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
-    let initialTimestamp, elapsedTime;
-    let animationFrameId;
-
     class Ball {
-      constructor(x, y, vx, vy, radius, color) {
-        this.x = x;
-        this.y = y;
-        this.vx = vx;
-        this.vy = vy;
+      constructor(canvas, radius, color) {
+        this.x = getRandomInt(0, canvas.width);
+        this.y = getRandomInt(0, canvas.height);
         this.radius = radius;
         this.color = color;
+        this.previousElapsedTime = 0;
+        this.isFirstDraw = true;
       }
-      draw(canvas, ctx) {
+      draw(canvas, ctx, elapsedTime, velocity) {
+        // Do nothing on first frame, because we need two frames to work out time since last draw
+        if (elapsedTime === 0) return;
+
+        /* Base animation speed on time rather than frames.
+         * This ensures speed stays independent of screen refresh rate, see:
+         * https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+         */
+        this.timeSinceLastDraw = elapsedTime - this.previousElapsedTime;
+
+        // Set initial speed and direction. Velocity is in pixels-per-second.
+        if (this.isFirstDraw) {
+          this.dx = (this.timeSinceLastDraw / 1000) * velocity;
+          this.dy = (this.timeSinceLastDraw / 1000) * velocity;
+          this.isFirstDraw = false;
+        }
+
+        // Move the drawing position
+        this.x += this.dx;
+        this.y += this.dy;
+
+        // Draw
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.closePath();
         ctx.fillStyle = this.color;
         ctx.fill();
-        this.x += this.vx;
-        this.y += this.vy;
 
-        if (this.y + this.vy > canvas.height || this.y + this.vy < 0) {
-          this.vy = -this.vy;
+        // On collision with edges, flip delta values for the next draw
+        if (this.y + this.dy > canvas.height || this.y + this.dy < 0) {
+          this.dy = -this.dy;
         }
-        if (this.x + this.vx > canvas.width || this.x + this.vx < 0) {
-          this.vx = -this.vx;
+        if (this.x + this.dx > canvas.width || this.x + this.dx < 0) {
+          this.dx = -this.dx;
         }
+
+        this.previousElapsedTime = elapsedTime;
       }
     }
 
-    const ball1 = new Ball(100, 100, 3, 2, 15, "blue");
-    const ball2 = new Ball(100, 100, 5, 3, 15, "red");
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    const ball1 = new Ball(canvas, 12, "blue");
+    const ball2 = new Ball(canvas, 12, "red");
+
+    let initialTimestamp, previousTimestamp, elapsedTime, animationFrameId;
 
     const renderFrame = (currentTimestamp) => {
       // If this is the first frame, store the initial timestamp
       if (initialTimestamp === undefined) {
         initialTimestamp = currentTimestamp;
-        // Else, calculate time since animation started – animation progress will be based on this
-      } else {
-        elapsedTime = currentTimestamp - initialTimestamp;
+      }
+      // Calculate time since animation started – animation progress will be based on this
+      elapsedTime = currentTimestamp - initialTimestamp;
+
+      if (previousTimestamp !== currentTimestamp) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ball1.draw(canvas, ctx, elapsedTime, 200);
+        ball2.draw(canvas, ctx, elapsedTime, 100);
       }
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ball1.draw(canvas, ctx);
-      ball2.draw(canvas, ctx);
-
+      previousTimestamp = currentTimestamp;
       // Step to next frame with recursive call
       animationFrameId = window.requestAnimationFrame(renderFrame);
     };
